527de4a12055ae1883d003b1fdae9c9b17b89340
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2019-10-04 20:36:04 -0600

    updating

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
+        question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        correctAnswer: "alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
+        question: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        correctAnswer: "parentheses"
     },
     {
-        title: "Arrays in JavaScript can be used to store:",
+        question: "Arrays in JavaScript can be used to store:",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
+        correctAnswer: "all of the above"
     },
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
+        question: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes"
+        correctAnswer: "quotes"
     },
     {
-        title: "A very useful tool used during development and debugging for pringitn content to the debugger is:",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        answer: "console.log"
+        correctAnswer: "console.log"
     }
-  ];
- 
+];
+
+var timeEl = document.querySelector(".iTime")
+
+var secondsLeft = 75;
+var timerInterval;
+
+function setTime() 
+{
+    timerInterval = setInterval(function() 
+    {
+        secondsLeft--;
+        timeEl.textContent = "Time Remaining: " + secondsLeft;
+        if(secondsLeft === 0) 
+        {
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+const startBtn = document.getElementById("start");
+startBtn.addEventListener('click', function()
+{
+    setTime();
+    startBtn.style.display = 'none';
+})
+
+function displayQuestions()
+{
+    document.getElementById("questions-dynamically-rendered-here").textContent = questions;
+}
+
+displayQuestions
